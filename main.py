65a20885cb53827fba5f0493a69334fba3077db5
Zindriana <hulten.johan22@gmail.com>
2025-01-17 21:54:05 +0100

    added containers and groups

--- main.py
+++ main.py
@@ -6,22 +6,27 @@ from constants import *
 from player import *
 
 def main():
-	pygame.init()
-	clock = pygame.time.Clock()
-	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-	dt = 0
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	while 1 > 0:
-		for event in pygame.event.get():
-    			if event.type == pygame.QUIT:
-        			return
-		screen.fill((0, 0, 0))
-		player.draw(screen)
-		player.update(dt)
-		pygame.display.flip()
-		dt = clock.tick(60)/1000
+    pygame.init()
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while 1 > 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
+        for updatable_object in updatable:
+            updatable_object.update(dt)
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
 if __name__ == "__main__":
     main()
